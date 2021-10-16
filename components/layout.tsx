31ac34e2ff956da6623be1becbdd4dd30ae0a47d
Nefomemes <nefothingy@hotmail.com>
2021-10-16 18:59:14 +0700

    feat: create layout for proper client-side navigation

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+import styles from "~/styles/Layout.module.scss";
+import NaviDrawer from "./navi-drawer";
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles["layout"]}>
+      <NaviDrawer />
+      <div className={styles["page-content"]}> {children} </div>
+
+      <footer className={styles.footer}>
+        <p>Created with <span className={styles["love-emoji"]}>♥️</span> by FortressValkyrie</p>
+        <p>Color pallete by the Dracula theme project. Hosted with Vercel.</p>
+       
+      </footer>
+    </div>
+  );
+}
