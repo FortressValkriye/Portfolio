1c09f1091238cbbe5abc11b04fa6756af6374946
Nefomemes <nefothingy@hotmail.com>
2021-11-05 13:49:25 +0700

    style: format things with Prettier

--- components/settings-popup.tsx
+++ components/settings-popup.tsx
@@ -3,24 +3,31 @@ import styles from "../styles/SettingsPopup.module.scss";
 import { Component } from "react";
 
 interface SettingsPopupProps {
-    onClose: (component?) => void;
+  onClose: (component?) => void;
 }
 interface SettingsPopupState {
-    theme: "dracula-night" | "dracula-light"; 
+  theme: "dracula-night" | "dracula-light";
 }
 
-export default class SettingsPopup extends Component<SettingsPopupProps, SettingsPopupState> {
-    constructor(props: SettingsPopupProps){
-        super(props);
-        this.onClose = props.onClose;
-        this.state = { theme: "dracula-light" };
-    };
-    onClose: SettingsPopupProps["onClose"];
+export default class SettingsPopup extends Component<
+  SettingsPopupProps,
+  SettingsPopupState
+> {
+  constructor(props: SettingsPopupProps) {
+    super(props);
+    this.onClose = props.onClose;
+    this.state = { theme: "dracula-light" };
+  }
+  onClose: SettingsPopupProps["onClose"];
 
-    render(){
-        return (<div className={`${styles["settings-popup-background"]}`}
-        onClick={() => this.onClose()}>
-            <div className={`${styles["settings-popup"]}`}></div>
-        </div>)
-    }
+  render() {
+    return (
+      <div
+        className={`${styles["settings-popup-background"]}`}
+        onClick={() => this.onClose()}
+      >
+        <div className={`${styles["settings-popup"]}`}></div>
+      </div>
+    );
+  }
 }
--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -1,65 +1,74 @@
-
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Layout from "~/components/layout";
 import { getMediumArticle } from "article-api";
 import styles from "~/styles/Newsletter.module.scss";
 import { randomUUID } from "crypto";
 interface APIArticle {
-    title: string,
-    description: string,
-    thumbnail?: string,
-    link: string,
-    categories: Array<string>,
-    publishedAt: string,
-
-};
+  title: string;
+  description: string;
+  thumbnail?: string;
+  link: string;
+  categories: Array<string>;
+  publishedAt: string;
+}
 
 interface Article {
-    title: string,
-    description: string,
-    thumbnail?: string,
-    link: string,
-    categories: Array<string>,
-    publishedAt: string;
-    uuid: string;
-};
+  title: string;
+  description: string;
+  thumbnail?: string;
+  link: string;
+  categories: Array<string>;
+  publishedAt: string;
+  uuid: string;
+}
 
-export function Article(props: {article: Article}){
-    return ( <div key={props.article.uuid}>
-        <h2><a href={props.article.link}>{ props.article.title }</a></h2>
-        <p>{ props.article.description }</p>
-        <p> { "Tagged: " } { props.article.categories.map((category) => <span className={styles["status-line"]}> { category } </span>) } </p>
-    </div>)
+export function Article(props: { article: Article }) {
+  return (
+    <div key={props.article.uuid}>
+      <h2>
+        <a href={props.article.link}>{props.article.title}</a>
+      </h2>
+      <p>{props.article.description}</p>
+      <p>
+        {" "}
+        {"Tagged: "}{" "}
+        {props.article.categories.map((category) => (
+          <span className={styles["status-line"]}> {category} </span>
+        ))}{" "}
+      </p>
+    </div>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps<{
-    posts: Array<Article>
-    }> = async (props) => {
-    
-        
-        return {
-        props: {
-            posts: [...(await getMediumArticle({
-                user: "Nefomemes"
-            }) as Array<Article>)].map((article) => {
-                article.publishedAt = article.publishedAt.toString();
-                article.uuid = randomUUID();
-                return article;
-            })
-        }
-    }
-}
+  posts: Array<Article>;
+}> = async (props) => {
+  return {
+    props: {
+      posts: [
+        ...((await getMediumArticle({
+          user: "Nefomemes",
+        })) as Array<Article>),
+      ].map((article) => {
+        article.publishedAt = article.publishedAt.toString();
+        article.uuid = randomUUID();
+        return article;
+      }),
+    },
+  };
+};
 
-export default function Newsletter({ posts } : InferGetServerSidePropsType<typeof getServerSideProps>){
-return (
-
-
-        <div className={styles.newsletter}>
-            <h1>Newsletter</h1>
-            <div className={styles["posts-list"]}>{   
-                posts.map((post) => <Article article={post}/>)
-            }</div>
-        </div>
-  
-)
+export default function Newsletter({
+  posts,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className={styles.newsletter}>
+      <h1>Newsletter</h1>
+      <div className={styles["posts-list"]}>
+        {posts.map((post) => (
+          <Article article={post} />
+        ))}
+      </div>
+    </div>
+  );
 }
