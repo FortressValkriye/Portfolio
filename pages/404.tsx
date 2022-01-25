5f27f126d986ffd7a239d89470e7def2aa282c25
Nefo Fortressia <nefothingy@hotmail.com>
2022-01-25 18:02:38 +0700

    refactor: set the h3 text in 404.tsx to a p text
    Also remove the unnecessary Layout import.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,9 @@
-import Layout from "~/components/layout";
 import styles from "~/styles/NotFound.module.scss";
 export default function NotFoundPage() {
   return (
     <div className={styles["not-found"]}>
       <h1>404</h1>
-      <h3>You found yourself in an unknown place.</h3>
+      <p>You found yourself in an unknown place.</p>
     </div>
   );
 }
