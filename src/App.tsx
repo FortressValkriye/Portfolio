c7d5de917965237ef715d952b4265f7da3302225
Nefo Fortressia <nefothingy@hotmail.com>
2022-05-18 20:04:16 +0700

    refactor: use WindiCSS for CSS things
    It's like Tailwind but faster!

--- src/App.tsx
+++ src/App.tsx
@@ -12,46 +12,34 @@ function App() {
           speed={0.8}
           factor={4.5}
           offset={0}
+          className="bg-[rgb(129,61,156)] z-[-1]"
           style={{
-            backgroundColor: "rgb(129,61,156)",
-            zIndex: -1,
             background:
               "linear-gradient(50deg, rgba(129,61,156,1) 23%, rgba(192,28,40,1) 62%, rgba(192,28,40,1) 68%, rgba(192,28,40,1) 94%, rgba(192,28,40,1) 96%)",
           }}
         ></ParallaxLayer>
-        <ParallaxLayer speed={0.8} offset={0} horizontal={true}>
+        <ParallaxLayer speed={0.8} offset={0} horizontal={true} className="w-screen ">
           <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "100vw",
-              height: "100vh",
-            }}
+            className="flex place-content-center w-screen h-screen"
+
           >
-            <div style={{ marginRight: "2rem" }}>
+            <div className="mr-8 flex place-content-center flex-col">
               <h1>Nefo Fortressia</h1>
               <h4>they/them</h4>
             </div>
-            <div style={{ marginLeft: "2rem" }}>
-              <img
-                style={{ borderRadius: "50rem" }}
+            <div className="flex items-center ">
+              <img className="mr-8 h-100 rounded-full"
                 src="https://avatars.githubusercontent.com/u/66000635?s=400&v=4"
               />
             </div>
           </div>
         </ParallaxLayer>
         <ParallaxLayer
-          style={{
-            display: "flex",
-          
-            justifyContent: "center",
-            alignItems: "center",
-          }}
+          className="flex place-content-center"
           offset={1}
           speed={1.5}
         >
-          <div>
+          <div className="text-center">
             <h2>
               Hiii!! I'm a developer working with Rust and the Linux desktop
               ecosystem
@@ -61,13 +49,13 @@ function App() {
               anything to do with Linux app development, just handle it to me.
             </p>
 
-            <div style={{ height: "4rem", position: "relative" }}>
+            <div className="flex place-content-center">
               <img
-                style={{ height: "100%" }}
+                className="h-20"
                 src="https://upload.wikimedia.org/wikipedia/commons/7/71/GTK_logo.svg"
               />
               <img
-                style={{ height: "100%" }}
+                className="h-20"
                 src="https://upload.wikimedia.org/wikipedia/commons/1/1a/Flatpak_logo.png"
               />
             </div>
@@ -75,19 +63,8 @@ function App() {
               Not only that, but I also do web technologies as well. Though...
               less frequently.
             </p>
-            <div style={{ height: "4rem", position: "relative" }}>
-              <img
-                style={{ height: "100%" }}
-                src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
-              ></img>
-              <img
-                style={{ height: "100%" }}
-                src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg"
-              ></img>
-              <img
-                style={{ height: "100%", filter: "invert(100%)" }}
-                src="https://simpleicons.org/icons/nextdotjs.svg"
-              ></img>
+            <div className="h-1 relative">
+
             </div>
           </div>
         </ParallaxLayer>
@@ -95,38 +72,34 @@ function App() {
           <div style={{
             display: "flex",
             justifyContent: "center",
-            alignItems: "center",  height: "100vh",
+            alignItems: "center", height: "100vh",
             zIndex: 3
           }}>
-          <div style={{width: "25vw"}}>
-            <h1>Echidna Code</h1>
-            <h3>
-              A cross-platform code editor that's designed to be user-friendly.
-              
-              
-            </h3>
-            <p>Designed for the GNOME desktop. Works on all systems.</p>
-            <a style={
-              {
-                color: "white",
-                fontSize: "2rem",
-                textDecoration: "none",
-                fontWeight: 600,
-                display: "flex",
-                alignItems: "center",
-                height: "2rem",
-                paddingTop: "5rem",
-                justifyContent: "center"
-               
+            <div style={{ width: "25vw" }}>
+              <h1>Echidna Code</h1>
+              <h3>
+                A cross-platform code editor that's designed to be user-friendly.
+
+
+              </h3>
+              <p>Designed for the GNOME desktop. Works on all systems.</p>
+              <a className="color-white h-8 pt-20 bold" style={
+                {
+                  color: "white",
+                  fontSize: "2rem",
+                  textDecoration: "none",
+
+
+
+                }
+              } href="https://github.com/fortressia/Echidna/"><img style={
+                { height: "2rem", filter: "invert(100%)", marginRight: "1rem" }
               }
-            } href="https://github.com/fortressia/Echidna/"><img style={
-             { height: "2rem", filter: "invert(100%)", marginRight: "1rem"}
-            }
-            src={gitSymbolicIcon}/><p>Learn More</p></a>
+                src={gitSymbolicIcon} /><p>Learn More</p></a>
+            </div>
+
+            <img style={{ marginTop: "3.5rem", marginLeft: "2rem" }} src={mockup} />
           </div>
-        
-            <img style={{marginTop: "3.5rem", marginLeft: "2rem"}} src={mockup}/>
-         </div>
 
 
         </ParallaxLayer>
@@ -134,7 +107,7 @@ function App() {
           speed={4}
           offset={2}
           factor={2}
-          
+
           style={{
             background:
               "linear-gradient(193deg, rgba(26,95,180,1) 36%, rgba(26,95,180,1) 39%, rgba(26,95,180,1) 39%, rgba(129,61,156,1) 74%, rgba(129,61,156,1) 90%, rgba(129,61,156,1) 94%, rgba(129,61,156,1) 96%)",
@@ -142,8 +115,8 @@ function App() {
             zIndex: -1
           }}
         ></ParallaxLayer>
-       
-      
+
+
         <ParallaxLayer
           offset={3}
           speed={2}
---
+++ windi.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
