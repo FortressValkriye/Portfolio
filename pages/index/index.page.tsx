dfc69045da0668f807533b7a5df51c8a392ea22a
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-13 21:13:23 +0700

    refactor: rewrite in vite-plugin-ssr

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Counter } from './Counter'
+/*
+ * Copyright 2022 Nefo Fortressia and contributors. All Rights Reserved
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
-export { Page }
 
-function Page() {
-  return (
-    <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-    </>
-  )
+import React from "react";
+
+export default function PortfolioPage() {
+    const isBrowser = typeof window !== 'undefined'
+    const isNodejs = !isBrowser
+
+    // We skip rendering the interactive map on the server-side (SSR'ing a map is mostly useless).
+    if (isNodejs) {
+        return <div>Loading...</div>
+    }
+
+    // We lazily load `<SomeHeavyMapComponent>`
+    // @ts-ignore
+    const ParallaxPage = React.lazy(() => import('./client-side'))
+    return (
+        <React.Suspense fallback={<div>Loading...</div>}>
+            <ParallaxPage/>
+        </React.Suspense>
+    )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
+import WindiCSS from "vite-plugin-windicss"
 
 const config: UserConfig = {
-  plugins: [react(), ssr()],
+  plugins: [react(), ssr(), WindiCSS({
+    scan: {
+      // By default only `src/` is scanned
+      dirs: ["pages"],
+      // We only have to specify the file extensions we actually use.
+      fileExtensions: ["js", "ts", "jsx", "tsx"]
+    }
+  })],
 }
 
 export default config
